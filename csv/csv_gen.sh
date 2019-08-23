cd42285c4df71976f4b4f90cf1802084e9857965
tinglu <lisalu729@gmail.com>
2019-08-23 16:49:16 +1000

    init

--- csv/csv_gen.sh
+++ csv/csv_gen.sh
@@ -2,34 +2,34 @@
 name="file1.csv"
 echo "$name"
 touch "$name"
-for i in {1..20}; do
+for i in {1..10}; do
     echo "$i" >> "$name"
 done
 
 name="file2.csv"
 echo "$name"
 touch "$name"
-for i in {1..30}; do
+for i in {1..20}; do
     echo "$i" >> "$name"
 done
 
 name="file3.csv"
 echo "$name"
 touch "$name"
-for i in {1..50}; do
+for i in {1..30}; do
     echo "$i" >> "$name"
 done
 
 name="file4.csv"
 echo "$name"
 touch "$name"
-for i in {1..20}; do
+for i in {1..40}; do
     echo "$i" >> "$name"
 done
 
 name="file5.csv"
 echo "$name"
 touch "$name"
-for i in {1..50}; do
+for i in {1..70}; do
     echo "$i" >> "$name"
 done
